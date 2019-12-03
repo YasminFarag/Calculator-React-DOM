ff2e3059af916a7dbca9550419ba644df978847c
yasmin <yasminefawzy22@gmail.com>
2019-12-03 14:57:56 +0100

    key enter

--- React-Calculator/src/App.js
+++ React-Calculator/src/App.js
@@ -7,64 +7,62 @@ import Screen from './component/Screen';
 export default class App extends Component {
   state = {
 
-        result: '',
-      }
+    result: '',
+  }
 
   calculate = () => {
 
-        try {
-          this.setState({
-            result: (eval(this.state.result) || "" ) + ""
-          })
-        } catch (e) {
-          this.setState({
-            result: 'Error',
-          })
-        }
+    try {
+      this.setState({
+        result: (eval(this.state.result) || "") + ""
+      })
+    } catch (e) {
+      this.setState({
+        result: 'Error',
+      })
+    }
 
 
   }
 
   reset = () => {
-        this.setState({
-          result: ''
-        })
+    this.setState({
+      result: ''
+    })
   }
 
-  backSpace=() => {
+  backSpace = () => {
     this.setState({
-      result: this.state.result.slice(0,-1)
+      result: this.state.result.slice(0, -1)
     });
 
   }
 
   onClick = (button) => {
-        if (button === 'C') {
-          this.reset()
-        } 
-        
-          else if (button === '=') {
-          this.calculate()
-        } 
-            else if(button === 'CE'){
-            this.backSpace()
+    if (button === 'C') {
+      this.reset()
+    } else if (button === '=' || button === 'Enter') {
+      this.calculate()
+    } else if (button === 'CE') {
+      this.backSpace()
 
-      }
-        
-        else {
-          this.setState({
-            result: this.state.result + button
-          })
-        }
-  }
-      render() {
-        return ( <div >
-          <Screen 
-          onClick={this.onClick}
-          result ={this.state.result} 
-          
-          / >
-          </div>
-        )
-      }
+    } else {
+      this.setState({
+        result: this.state.result + button
+      })
     }
+  }
+  render() {
+    return (
+       <div>
+      
+      <Screen onClick = {
+        this.onClick
+      }
+      result = {
+        this.state.result
+      } /> 
+      </div >
+    )
+  }
+}
